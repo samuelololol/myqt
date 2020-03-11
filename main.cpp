5f2ff2d805d4e19bcf5d055e883ab6acf79426c1
samuelololol <samuelololol@gmail.com>
2020-03-12 01:26:09 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+#include <QApplication>
+#include <QPushButton>
+
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    QPushButton button ("hello world !");
+    button.show();
+
+    return app.exec();
+}
